8dbf170b845628d6f596764ee3b2b89c9e80d211
jamilshafayat <jamilshafayat8@gmail.com>
2021-07-15 18:37:23 +0600

    top section design

--- src/app/components/navbar/navItems.tsx
+++ src/app/components/navbar/navItems.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
-import styled from 'styled-components'
-import tw from 'twin.macro';
+import React from "react";
+import { useMediaQuery } from "react-responsive";
+import styled, { css } from "styled-components";
+import tw from "twin.macro";
+import { SCREENS } from "../responsive";
+import { slide as Menu } from "react-burger-menu";
+import menuStyles from "./menuStyles";
 
 const ListContainer = styled.ul`
   ${tw`
@@ -9,7 +13,7 @@ const ListContainer = styled.ul`
   `};
 `;
 
-const NavItem = styled.li`
+const NavItem = styled.li<{ menu?: any }>`
   ${tw`
     text-xs
     md:text-base
@@ -23,25 +27,56 @@ const NavItem = styled.li`
     ease-in-out
     hover:text-gray-700
   `};
+
+  ${({ menu }) => menu && css`
+    ${tw`
+      text-white    
+      text-xl
+      mb-3
+      focus: text-white
+    `};
+  `};
 `;
 
 const NavItems = () => {
+  const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+
+  if (isMobile)
+    return (
+      <Menu right styles={menuStyles}>
+        <ListContainer>
+          <NavItem menu>
+            <a href="/">Home</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="/">Cars</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="/">Services</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="/">Contact Us</a>
+          </NavItem>
+        </ListContainer>
+      </Menu>
+    );
+
   return (
     <ListContainer>
       <NavItem>
-        <a href="#">Home</a>
+        <a href="/">Home</a>
       </NavItem>
       <NavItem>
-        <a href="#">Cars</a>
+        <a href="/">Cars</a>
       </NavItem>
       <NavItem>
-        <a href="#">Services</a>
+        <a href="/">Services</a>
       </NavItem>
       <NavItem>
-        <a href="#">Contact Us</a>
+        <a href="/">Contact Us</a>
       </NavItem>
     </ListContainer>
-  )
-}
+  );
+};
 
-export default NavItems
+export default NavItems;
